e50236328742728b2db61e0556d9b7e5e2f494f6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-26 10:26:07 -0700

    Add more scenarios to add stock method to beef up test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -27,18 +27,18 @@ class VendorTest < Minitest::Test
 
     assert_equal expected, @vendor.inventory
     assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+
+    expected2 = {@item1 => 55, @item2 => 12}
+
+    assert_equal expected2, @vendor.inventory
   end
 
 end
 
-
-# #=> 30
 #
-# @vendor.stock(@item1, 25)
 #
-# @vendor.check_stock(@item1)
-# #=> 55
-#
-# @vendor.stock(item2, 12)
-#
-# @vendor.inventory
