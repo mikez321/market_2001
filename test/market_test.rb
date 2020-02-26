8e7359c31e0f49aec3a1b912680464659c75dad6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-26 10:39:42 -0700

    Create add vendor method and test

--- test/market_test.rb
+++ test/market_test.rb
@@ -15,6 +15,11 @@ class MarketTest < Minitest::Test
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @vendor1.stock(@item1, 35)
     @vendor1.stock(@item2, 7)
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor3.stock(@item1, 65)
   end
 
   def test_it_exists
@@ -26,4 +31,31 @@ class MarketTest < Minitest::Test
     assert_equal "South Pearl Street Farmers Market", market.name
     assert_equal [], market.vendors
   end
+
+  def test_it_can_add_vendors
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+
+  end
 end
+#
+# @market.vendor_names
+# #=> ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+#
+# @market.vendors_that_sell(item1)
+# #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
+#
+# @market.vendors_that_sell(item4)
+# #=> [#<Vendor:0x00007fe1349bed40...>]
+#
+# @vendor1.potential_revenue
+# #=> 29.75
+#
+# @vendor2.potential_revenue
+# #=> 345.00
+#
+# @vendor3.potential_revenue
+# #=> 48.75
