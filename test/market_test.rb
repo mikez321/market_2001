d7e6bae2a26450cd44ef229a3708fff47a9b3a71
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-26 10:31:33 -0700

    Create test for market class

---
+++ test/market_test.rb
@@ -0,0 +1,29 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+require 'minitest/test'
+require 'minitest/pride'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1.stock(item1, 35)
+    @vendor1.stock(item2, 7)
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+end
