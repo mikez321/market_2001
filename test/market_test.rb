0d6dabbc903e79c02af6b4e19263e4ba84b8f1e8
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-26 10:36:10 -0700

    Get market test working and create first market tests

--- test/market_test.rb
+++ test/market_test.rb
@@ -1,11 +1,11 @@
-require './lib/item'
-require './lib/vendor'
 require './lib/market'
+require './lib/vendor'
+require './lib/item'
+require 'minitest/autorun'
 require 'minitest/test'
 require 'minitest/pride'
 
 class MarketTest < Minitest::Test
-
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
@@ -13,8 +13,8 @@ class MarketTest < Minitest::Test
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @vendor1.stock(item1, 35)
-    @vendor1.stock(item2, 7)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
   end
 
   def test_it_exists
