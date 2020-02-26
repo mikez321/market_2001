f2d6aad83e95e3c2d9c6d336efe6db89b477d90c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-26 10:06:42 -0700

    Create test for item

---
+++ test/item_test.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+require './lib/vendor'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ItemTest < Minitest::Test
+
+  def setup
+    @tem1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists
+    assert_instance_of Item, @item1
+  end
+
+  def test_it_has_attributes
+    assert_equal "Tomato", item2.name
+    assert_equal 0.50, item2.price
+  end
+end
